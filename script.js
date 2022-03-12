32911867a5fd7a8f2424c7d991c6cbd932e24aaa
SivaTeja-20 <99390827+SivaTeja-20@users.noreply.github.com>
2022-03-12 21:34:10 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// declaration  of var
+var nam = "Teja"
+var age = 20
+var gpa = 8.2
+var bool = true
+
+console.log("Student Name : " + nam)
+console.log("Student Age : " + age)
+console.log("Student Cgpa : " + gpa)
+console.log(bool)
+
+
+// Radius of circle 
+var r = 10
+
+let pi = 3.14
+
+const c = 2*pi*r
+const a = pi*r*r
+console.log("Radius of a circle is : " +r)
+console.log("Circumference of circle is : " +c)
+console.log("Area of circle of is : " +a)
+
+
